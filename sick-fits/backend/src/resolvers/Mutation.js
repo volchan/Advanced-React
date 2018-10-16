74690da1d7aa95ca5a3eada5c09e0c816882bb9d
stephane de la torre <s.dlt@hotmail.fr>
2018-10-16 19:20:27 +0200

    13/68 done

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, { name }, ctx, info) {
+    global.dogs = global.dogs || [];
+    const newDog = { name };
+    global.dogs.push(newDog);
+    return newDog;    
+  }
+};
 
 module.exports = Mutations;
