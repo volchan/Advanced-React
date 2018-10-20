4dd27c6fd6fb1585ee3296b38d787435fb1fd2d6
stephane de la torre <s.dlt@hotmail.fr>
2018-10-20 18:17:06 +0200

    20/68 done

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -20,6 +20,15 @@ const Mutations = {
       data: updates,
       where: { id: args.id }
     }, info);
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // check if they own that item, or have the permission
+    // TODO
+    // delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
