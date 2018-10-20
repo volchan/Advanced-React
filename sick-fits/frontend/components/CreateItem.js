e89d9714afd763e7888109d7fa662fb3bbb9a26e
stephane de la torre <s.dlt@hotmail.fr>
2018-10-20 15:52:21 +0200

    18/68 done

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import Router from "next/router";
+import axios from "axios";
 
 import Form from "./styles/Form";
 import formatMoney from "../lib/formatMoney";
@@ -42,6 +43,21 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    const files = e.target.files;
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    formData.append("upload_preset", "sick-fits");
+    const { data, status, statusText } = await axios.post(
+      "https://api.cloudinary.com/v1_1/volchan-main/image/upload",
+      formData
+    );
+    this.setState({
+      image: data.secure_url,
+      largeImage: data.eager[0].secure_url
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -61,6 +77,25 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  accept="image/*"
+                  onChange={this.uploadFile}
+                  required
+                />
+                {this.state.image && (
+                  <img
+                    width="200"
+                    src={this.state.image}
+                    alt="Upload Preview"
+                  />
+                )}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
