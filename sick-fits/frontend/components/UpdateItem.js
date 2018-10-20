1b5254c9f73c0549aad3be0b1af744ba5bca65fc
stephane de la torre <s.dlt@hotmail.fr>
2018-10-20 16:37:02 +0200

    19/68 done

---
+++ sick-fits/frontend/components/UpdateItem.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import { Mutation, Query } from "react-apollo";
+import gql from "graphql-tag";
+import Router from "next/router";
+import axios from "axios";
+
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+
+const UPDATE_ITEM_MUTATION = gql`
+  mutation UPDATE_ITEM_MUTATION(
+    $id: ID!
+    $title: String
+    $description: String
+    $price: Int
+  ) {
+    updateItem(id: $id,title: $title, description: $description, price: $price) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+
+class UpdateItem extends Component {
+  state = {};
+
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
+  updateItem = async (e, updateItemMutation) => {
+    e.preventDefault();
+    const res = await updateItemMutation({
+      variables: {
+        id: this.props.id,
+        ...this.state
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {({ data, loading }) => {
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No Item found.</p>
+          return (
+            <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+              {(updateItem, { loading, error }) => (
+                <Form onSubmit={e => this.updateItem(e, updateItem)}>
+                  <Error error={error} />
+                  <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        defaultValue={data.item.title}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </label>
+                    <label htmlFor="price">
+                      Price
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        defaultValue={data.item.price}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </label>
+                    <label htmlFor="description">
+                      Description
+                      <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Description"
+                        defaultValue={data.item.description}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </label>
+                    <button type="submit">Sav{loading ? "ing" : "e"} changes</button>
+                  </fieldset>
+                </Form>
+              )}
+            </Mutation>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default UpdateItem;
+export { UPDATE_ITEM_MUTATION };
---
+++ sick-fits/frontend/pages/update.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+import UpdateItem from "../components/UpdateItem";
+
+const Update = ({ query }) => (
+  <div>
+    <UpdateItem id={query.id} />
+  </div>
+);
+
+export default Update;
