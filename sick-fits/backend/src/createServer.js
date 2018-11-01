98bbec0d345a28c9639bd26dfb090bf3876ea245
stephane de la torre <s.dlt@hotmail.fr>
2018-11-01 22:24:39 +0100

    Fix for heroku

--- sick-fits/backend/src/createServer.js
+++ sick-fits/backend/src/createServer.js
@@ -1,8 +1,8 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const Query = require("./resolvers/query");
-const Mutation = require("./resolvers/mutation");
-const db = require("./db");
+const Query = require('./resolvers/query');
+const Mutation = require('./resolvers/mutation');
+const db = require('./db');
 
 // Create the GraphQl Yoga server
 function createServer() {
